67f33c17d639499b0aeb3534c30003dcf5aa36c1
Nicholas Walker <niwalker@westmont.edu>
2021-05-17 00:47:51 -0700

    day-4

--- car_inventory/__init__.py
+++ car_inventory/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from config import Config
 from .site.routes import site
 from .authentication.routes import auth
+from.api.routes import api
 from flask_migrate import Migrate
-from car_inventory.models import db as root_db, login_manager
+from car_inventory.models import db as root_db, login_manager, ma
 
-
+# CORS - cross origin recourse sharing
+from flask_cors import CORS
 
 app = Flask(__name__)
 
@@ -13,11 +15,16 @@ app.config.from_object(Config)
 
 app.register_blueprint(site)
 app.register_blueprint(auth)
+app.register_blueprint
 
 root_db.init_app(app)
 migrate = Migrate(app, root_db)
 
 login_manager.init_app(app)
-#login_manager.login_view = 'signin'
 
+login_manager.login_view = 'auth.signin'
+
+
+ma.init_app(app)
+CORS(app)
 from car_inventory import models
--- car_inventory/models.py
+++ car_inventory/models.py
@@ -11,9 +11,13 @@ import secrets
 
 from flask_login import UserMixin, LoginManager
 
+#Install Our Marshaller
+from flask_marshmallow import Marshmallow
+
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+ma = Marshmallow()
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -28,6 +32,7 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String, nullable = False, default = '')
     token = db.Column(db.String, default = '', unique = True)
     date_created = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    car = db.relationship('Car', backref = 'owner', lazy = True)
 
     def __init__(self, email, first_name = '', last_name = '', id = '', password = '', token = ''):
         self.id = self.set_id()
@@ -49,3 +54,49 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f"User: {self.email} has been created and added to the database!"
+
+
+class Car(db.Model):
+    id = db.Column(db.String, primary_key = True)
+    name = db.Column(db.String(150))
+    description = db.Column(db.String(200), nullable = True)
+    price = db.Column(db.Numeric(precision = 10, scale = 2))
+    rear_view_camera = db.Column(db.String(120), nullable = True)
+    miles_per_trip = db.Column(db.String(100), nullable = True)
+    max_speed = db.Column(db.String(100))
+    dimensions = db.Column(db.String(100))
+    weight = db.Column(db.String(50))
+    cost_of_custom = db.Column(db.Numeric(precision = 10, scale = 2))
+    make = db.Column(db.String(150))
+    user_token = db.Column(db.String, db.ForeignKey('user.token'), nullable = False)
+
+    def __init__(self, name, description, price, rear_view_camera, miles_per_trip, max_speed, dimensions, weight, cost_of_custom, make, user_token, id = ''):
+            self.id = self.set_id()
+            self.name = name
+            self.description = description
+            self.price = price
+            self.rear_view_camera = rear_view_camera
+            self.miles_per_trip = miles_per_trip
+            self.max_speed = max_speed
+            self.dimensions = dimensions
+            self.weight = weight
+            self.cost_of_custom = cost_of_custom
+            self.make = make
+            self.user_token = user_token
+
+    def __repr__(self):
+        return f'The following car has been created: {self.name}'
+    
+    def set_id(self):
+        return str(uuid.uuid4())
+
+# API Schema via Marshmallow
+class CarSchema(ma.Schema):
+    class Meta:
+        fields = ['id', 'name', 'description', 'price', 'rear_view_camera', 'miles_per_trip', 'max_speed', 'dimensions', 'weight', 'cost_of_custom', 'make']
+
+# Singular data point return
+car_schema = CarSchema()
+
+# List of multiple onjects returned
+car_schemas = CarSchema(many = True)
